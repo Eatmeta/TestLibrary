d53ada83e2ac9a211dd481a414462bca02635f9b
Eatmeta <metagroul@gmail.com>
2023-03-08 18:36:20 +0300

    Add data to seed Identity and IdentityServer databases

--- IdentityServer6/Program.cs
+++ IdentityServer6/Program.cs
@@ -1,4 +1,7 @@
 using System.Reflection;
+using Duende.IdentityServer.EntityFramework.DbContexts;
+using Duende.IdentityServer.EntityFramework.Mappers;
+using Duende.IdentityServer.Models;
 using IdentityServer6.Data;
 using IdentityServer6.Models;
 using Microsoft.AspNetCore.Identity;
@@ -30,6 +33,78 @@ builder.Services.AddIdentityServer()
     });
 
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    using var scope = app.Services.CreateScope();
+
+    await scope.ServiceProvider.GetRequiredService<ApplicationDbContext>().Database.MigrateAsync();
+    await scope.ServiceProvider.GetRequiredService<ConfigurationDbContext>().Database.MigrateAsync();
+    await scope.ServiceProvider.GetRequiredService<PersistedGrantDbContext>().Database.MigrateAsync();
+
+    var userManager = scope.ServiceProvider.GetRequiredService<UserManager<ApplicationUser>>();
+
+    if (await userManager.FindByNameAsync("admin") == null)
+    {
+        await userManager.CreateAsync(
+            new ApplicationUser
+            {
+                UserName = "admin",
+                Email = "admin@example.com"
+            }, "Password123+");
+    }
+    
+    var configurationDbContext = scope.ServiceProvider.GetRequiredService<ConfigurationDbContext>();
+    
+    if (!await configurationDbContext.ApiResources.AnyAsync())
+    {
+        await configurationDbContext.ApiResources.AddAsync(new ApiResource
+        {
+            Name = Guid.NewGuid().ToString(),
+            DisplayName = "API",
+            Scopes = new List<string> {"https://www.example.com/api"}
+        }.ToEntity());
+
+        await configurationDbContext.SaveChangesAsync();
+    }
+
+    if (!await configurationDbContext.ApiScopes.AnyAsync())
+    {
+        await configurationDbContext.ApiScopes.AddAsync(new ApiScope
+        {
+            Name = "https://www.example.com/api",
+            DisplayName = "API"
+        }.ToEntity());
+
+        await configurationDbContext.SaveChangesAsync();
+    }
+
+    if (!await configurationDbContext.Clients.AnyAsync())
+    {
+        await configurationDbContext.Clients.AddRangeAsync(
+            new Client
+            {
+                ClientId = Guid.NewGuid().ToString(),
+                ClientSecrets = new List<Secret> {new("secret".Sha512())},
+                ClientName = "Admin",
+                AllowedGrantTypes = GrantTypes.ClientCredentials,
+                AllowedScopes = new List<string> {"https://www.example.com/api"},
+                AllowedCorsOrigins = new List<string> {"https://api:7001"}
+            }.ToEntity());
+
+        await configurationDbContext.SaveChangesAsync();
+    }
+
+    if (!await configurationDbContext.IdentityResources.AnyAsync())
+    {
+        await configurationDbContext.IdentityResources.AddRangeAsync(
+            new IdentityResources.OpenId().ToEntity(),
+            new IdentityResources.Profile().ToEntity(),
+            new IdentityResources.Email().ToEntity());
+
+        await configurationDbContext.SaveChangesAsync();
+    }
+}
 
 app.Run();
 
